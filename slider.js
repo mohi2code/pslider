18309c6c17eaed9f45c1a6959cd8489b5c1571de
MohiEldin Ahmed <mohi2code@MohiEldins-MacBook-Air.local>
2022-06-12 14:46:16 +0300

    :sparkles: moving slider responsive behavior to js

--- slider.js
+++ slider.js
@@ -1,14 +1,15 @@
-let slider = document.querySelector("#slider")
-let currImage = document.currImage
+let slider      = document.querySelector("#slider")
+let sliderImage = document.querySelector(".slider-image")
+let currImage   = document.currImage
 let larrow = document.querySelector(".larrow")
 let rarrow = document.querySelector(".rarrow")
 
 images = ["/images/ape1.jpeg", "/images/ape2.jpeg"]
 let imageIndex = 0
 
-currImage.src = images[imageIndex]
+currImage.src = images[imageIndex]  // set initial image
 
-rarrow.onclick = ()=>{
+rarrow.onclick = ()=>{  // right arrow onclick listener 
     if (imageIndex+1 > images.length-1)
         imageIndex = 0
     else 
@@ -17,7 +18,7 @@ rarrow.onclick = ()=>{
     updateCurrImage()
 }
 
-larrow.onclick = ()=>{
+larrow.onclick = ()=>{  // left arrow onclick listener
     if (imageIndex-1 < 0)
         imageIndex = images.length-1
     else 
@@ -26,6 +27,49 @@ larrow.onclick = ()=>{
     updateCurrImage()
 }
 
+// update current image "src" attribute
 function updateCurrImage() {
     currImage.src = images[imageIndex]
 }
+
+/*
+ *  handle responsive behavior
+ */
+
+// mobile devices
+let mediaQueryXS = window.matchMedia('(max-width: 567px)')
+mediaQueryXS.addEventListener("change", xs)
+
+function xs(e) {
+    if (e.matches) {
+        sliderImage.style = "width: 100%"
+        currImage.style = "border-radius: 0"
+    }
+}
+
+xs(mediaQueryXS)
+
+// tablets
+let mediaQueryMedium = window.matchMedia('(min-width: 567px) and (max-width: 768px)')
+mediaQueryMedium.addEventListener("change", medium)
+
+function medium(e) {
+    if (e.matches) {
+        sliderImage.style = "width: 75%"
+        currImage.style = "border-radius: 1rem"
+    }
+}
+
+medium(mediaQueryMedium)
+ 
+
+// large devices
+let mediaQueryLarge = window.matchMedia('(min-width: 768px)')
+mediaQueryLarge.addEventListener("change", large)
+
+function large(e) {
+    if (e.matches)
+        sliderImage.style = "width: 50%"
+}
+
+large(mediaQueryLarge)

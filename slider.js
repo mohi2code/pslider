6cc8bd420f7e2e38aa321d91d8bd813d5de0bb00
MohiEldin Ahmed <mohi2code@MohiEldins-MacBook-Air.local>
2022-06-12 03:57:21 +0300

    :spoon: Functionality

--- slider.js
+++ slider.js
@@ -1,1 +1,31 @@
 let slider = document.querySelector("#slider")
+let currImage = document.currImage
+let larrow = document.querySelector(".larrow")
+let rarrow = document.querySelector(".rarrow")
+
+images = ["/images/ape1.jpeg", "/images/ape2.jpeg"]
+let imageIndex = 0
+
+currImage.src = images[imageIndex]
+
+rarrow.onclick = ()=>{
+    if (imageIndex+1 > images.length-1)
+        imageIndex = 0
+    else 
+        imageIndex++
+
+    updateCurrImage()
+}
+
+larrow.onclick = ()=>{
+    if (imageIndex-1 < 0)
+        imageIndex = images.length-1
+    else 
+        imageIndex--
+
+    updateCurrImage()
+}
+
+function updateCurrImage() {
+    currImage.src = images[imageIndex]
+}
